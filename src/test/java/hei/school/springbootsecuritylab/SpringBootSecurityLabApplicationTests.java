79c9e5d55466e8b151547afb59fa1b131aa9195c
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-08-12 17:10:26 +0300

    feat: configure Spring Security with basic authentication

---
+++ src/test/java/hei/school/springbootsecuritylab/SpringBootSecurityLabApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.springbootsecuritylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
